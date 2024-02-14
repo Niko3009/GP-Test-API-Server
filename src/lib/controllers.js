22418724ca52d00fc9075bdbdd9ca1d6b842a3ab
Nikita-Pet <nikita_petrishchev94@mail.ru>
2024-02-14 03:39:17 +0300

    V3-14-02

--- src/lib/controllers.js
+++ src/lib/controllers.js
@@ -14,33 +14,41 @@ const availableTypes = ["mistake", "remark", "recommendation"];
 const availableStatuses = ["waiting", "processing", "ready"];
 
 export const getAllAppeals = async (req, res) => {
-  const data = await readData();
-  logger.info(`controller getAllAppeals`);
-  res.status(200).send(getResData(data));
+  try {
+    const data = await readData();
+    logger.info(`controller getAllAppeals`);
+    res.status(200).send(getResData(data));
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
 
 export const addAppeal = async (req, res) => {
-  const body = req.body;
+  try {
+    const body = req.body;
 
-  const id = uniqid();
-  const date = new Date();
-  const autor = body?.autor;
-  const description = body?.description;
+    const id = uniqid();
+    const date = new Date();
+    const autor = body?.autor;
+    const description = body?.description;
 
-  const isAvailableType = availableTypes.includes(body?.type);
-  const type = isAvailableType ? body?.type : availableTypes[0];
+    const isAvailableType = availableTypes.includes(body?.type);
+    const type = isAvailableType ? body?.type : availableTypes[0];
 
-  const isAvailableStatus = availableStatuses.includes(body?.status);
-  const status = isAvailableStatus ? body?.status : availableStatuses[0];
+    const isAvailableStatus = availableStatuses.includes(body?.status);
+    const status = isAvailableStatus ? body?.status : availableStatuses[0];
 
-  const newAppeal = { id, date, autor, type, description, status };
+    const newAppeal = { id, date, autor, type, description, status };
 
-  const oldAppealsList = await readData();
-  const newAppealsList = [...oldAppealsList, newAppeal];
+    const oldAppealsList = await readData();
+    const newAppealsList = [...oldAppealsList, newAppeal];
 
-  const newData = await writeData(newAppealsList);
+    const newData = await writeData(newAppealsList);
 
-  logger.info(`controller addAppeal`);
-  logger.info(JSON.stringify(body));
-  res.status(200).send(getResData(newData));
+    logger.info(`controller addAppeal`);
+    logger.info(JSON.stringify(body));
+    res.status(200).send(getResData(newData));
+  } catch (err) {
+    res.status(500).send(err);
+  }
 };
