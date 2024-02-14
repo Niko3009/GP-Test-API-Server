f42ac89679963ecb350753fd654c708b6a8c28a6
Nikita-Pet <nikita_petrishchev94@mail.ru>
2024-02-15 01:08:09 +0300

    V10-12-02

--- src/lib/dbActions.ts
+++ src/lib/dbActions.ts
@@ -4,15 +4,22 @@ const { readFile, writeFile } = fsPromises;
 
 const dbFile = "./src/database/appeals.json";
 
-// const options = { encoding: "utf-8" };
-
 export const readData = async () => {
   const data = JSON.parse(String(await readFile(dbFile)));
   return data;
 };
 
-export const writeData = async (newData) => {
+export const writeData = async (newData: AppealType[]) => {
   await writeFile(dbFile, JSON.stringify(newData));
   const newFileData = await readData();
   return newFileData;
 };
+
+interface AppealType {
+  id: string;
+  date: Date;
+  autor: string;
+  type: string;
+  description: string;
+  status: string;
+}
